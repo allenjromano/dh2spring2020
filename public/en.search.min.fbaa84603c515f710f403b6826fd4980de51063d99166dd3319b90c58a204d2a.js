a0368347dec4956d6ef7f886d44004c4814ae440
ajr <aromano@fsu.edu>
2020-01-13 10:40:32 -0500

    static sites part 2

---
+++ public/en.search.min.fbaa84603c515f710f403b6826fd4980de51063d99166dd3319b90c58a204d2a.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.17f46cf4d2d10860bdb574318428807a2b82d804634a8c479f8a0c5bf0df565d.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
